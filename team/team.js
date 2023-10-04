545dfdbb9a286fb4d276fcf38f0b3624c85bb75b
TejasMishr <tejasmishra72@gmail.com>
2023-10-04 13:11:42 +0530

    github icon updated navbar responsive done

--- team/team.js
+++ team/team.js
@@ -3,6 +3,7 @@ function addContributor(imageUrl, contributorName, githubLink, linkedinLink) {
 
     var contributorCard = document.createElement('div'); //duv created with class card
     contributorCard.className = 'card';
+    contributorCard.style.backgroundColor = '#947f7f';
 
     var imgSection = document.createElement('div');
     imgSection.className = 'img-sec'; //image 
@@ -33,8 +34,10 @@ function addContributor(imageUrl, contributorName, githubLink, linkedinLink) {
     githubLinkElement.href = githubLink;
     githubLinkElement.target = '_blank';
 
+
     var githubIcon = document.createElement('i');
     githubIcon.className = 'fa-brands fa-github fa-2x hero';
+    githubIcon.className = 'fa-brands fa-github fa-2x hero github-icon';
     githubLinkElement.appendChild(githubIcon);
 
     //linkedin
@@ -66,7 +69,7 @@ var contributorName = 'Sarthak Vishwakarma';
 var githubLink = 'https://github.com/hackbysarthak03';
 var linkedin = 'https://www.linkedin.com/in/hellosarthak/';
 addContributor(imageUrl, contributorName, githubLink);
-var imageUrl = 'https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png';
+var imageUrl = 'https://akm-img-a-in.tosshub.com/indiatoday/styles/medium_crop_simple/public/2023-05/aman.png?VersionId=SgKyFgL3Zkh.iJ20x8TlQ95Fq8uSB.7e';
 var contributorName = 'Tejas Mishra';
 var githubLink = 'https://github.com/Tejasmishr';
 var linkedin='https://www.linkedin.com/in/tejasmishr/'
@@ -86,29 +89,29 @@ addContributor(imageUrl, contributorName, githubLink,linkedin);
 
 // -- >  addContributor(Url Of Image, contributor Name (Your Name), github Account Link , linkedin account link);
 
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Aditya Gaur","https://github.com/AdityaGaur7","#")
+addContributor("https://images.nightcafe.studio/jobs/c4ZvCMkQlgsakBNyks7n/c4ZvCMkQlgsakBNyks7n--2--a57o7_2x.jpg?tr=w-1600,c-at_max","Aditya Gaur","https://github.com/AdityaGaur7","#")
 
-addContributor("https://cdn.pixabay.com/photo/2021/08/17/09/03/girl-6552421_1280.png","Aaradhaya Singh","https://github.com/aaradhayasingh811","#")
+// addContributor("https://cdn.pixabay.com/photo/2021/08/17/09/03/girl-6552421_1280.png","Aaradhaya Singh","https://github.com/aaradhayasingh811","#")
 
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Meet Thakur","https://github.com/MeetThakur","#")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Visa Coder","https://www.github.com/codeWithVisa","#")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Nalukui malimba","https://github.com/nalukui01","#")
-addContributor("https://cdn.pixabay.com/photo/2021/08/17/09/03/girl-6552421_1280.png","Anjali Gupta","#")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Hrishap Joshi","https://github.com/Hrishap")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Md Faizan Khan","https://github.com/thefznkhan")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Saleh Ahmed","https://github.com/SalehAhmed10")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Bishal Roy","https://github.com/R0Y15")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Saurabh Singh","https://github.com/saurabhsingh720")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Mateo Álvarez","https://github.com/drifterDev")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Ankit Goyal","https://github.com/Ankit9126")
-addContributor("https://cdn.pixabay.com/photo/2021/08/17/09/03/girl-6552421_1280.png","Varsha Awasthi","#")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png"," Shubham Goyal","https://github.com/ShubhamGoyal7125")
-addContributor("https://cdn.pixabay.com/photo/2021/08/17/09/03/girl-6552421_1280.png","Aastha Gupta","https://github.com/motivated-star")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Prakash Mondal","https://github.com/rko0211")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Deepak Maurya","https://github.com/deepakmaur")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Kartik Labhshetwar","https://github.com/KartikLabhshetwar")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Meet Thakur","https://github.com/MeetThakur","#")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Visa Coder","https://www.github.com/codeWithVisa","#")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Nalukui malimba","https://github.com/nalukui01","#")
+// addContributor("https://cdn.pixabay.com/photo/2021/08/17/09/03/girl-6552421_1280.png","Anjali Gupta","#")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Hrishap Joshi","https://github.com/Hrishap")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Md Faizan Khan","https://github.com/thefznkhan")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Saleh Ahmed","https://github.com/SalehAhmed10")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Bishal Roy","https://github.com/R0Y15")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Saurabh Singh","https://github.com/saurabhsingh720")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Mateo Álvarez","https://github.com/drifterDev")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Ankit Goyal","https://github.com/Ankit9126")
+// addContributor("https://cdn.pixabay.com/photo/2021/08/17/09/03/girl-6552421_1280.png","Varsha Awasthi","#")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png"," Shubham Goyal","https://github.com/ShubhamGoyal7125")
+// addContributor("https://cdn.pixabay.com/photo/2021/08/17/09/03/girl-6552421_1280.png","Aastha Gupta","https://github.com/motivated-star")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Prakash Mondal","https://github.com/rko0211")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Deepak Maurya","https://github.com/deepakmaur")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Kartik Labhshetwar","https://github.com/KartikLabhshetwar")
 
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Piyush Kumar Tyagi","https://github.com/piyushtyagiofficial")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Dhiraj Laulkar","https://github.com/githubdhiraj")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Reajul Haque Reayz","https://github.com/Reayz")
-addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Bhushan K   ","https://github.com/KBhushan07","#")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Piyush Kumar Tyagi","https://github.com/piyushtyagiofficial")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Dhiraj Laulkar","https://github.com/githubdhiraj")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Reajul Haque Reayz","https://github.com/Reayz")
+// addContributor("https://cdn.pixabay.com/photo/2017/01/31/21/23/   avatar-2027365_1280.png","Bhushan K   ","https://github.com/KBhushan07","#")
